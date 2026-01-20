9ef8ae7ed66e3fb57d90c769c23333d14dbb1c68
Iqra-qazi <iqraq577@gmail.com>
2026-01-20 23:38:41 +0500

    js

--- chapter no 21-25/app.js
+++ chapter no 21-25/app.js
@@ -1,16 +1,16 @@
 // Question no 1
-// var firstname =prompt("Enter your first name")
-// var lastname =prompt("Enter your last name")
-// var firstChar=firstname.slice(0,1)
-// var remainingChar= firstname.slice(1)
-// var titleCase = firstChar .toUpperCase() + remainingChar.toLowerCase()+ " "+lastname
+var firstname =prompt("Enter your first name")
+var lastname =prompt("Enter your last name")
+var firstChar=firstname.slice(0,1)
+var remainingChar= firstname.slice(1)
+var titleCase = firstChar .toUpperCase() + remainingChar.toLowerCase()+ " "+lastname
 
-// document.write("WelCome " + titleCase);
+document.write("WelCome " + titleCase);
 
 //Question no 2
-// var favMob = prompt("Enter your favourite mobilephone with model");
-// document.write("My favourit mobilephone is " + favMob +"<br>" )
-// document.write("length of string " +favMob.length)
+var favMob = prompt("Enter your favourite mobilephone with model");
+document.write("My favourit mobilephone is " + favMob +"<br>" )
+document.write("length of string " +favMob.length)
 
 // Question no 3
 var word ="Pakistani"
@@ -29,27 +29,27 @@ document.write("String :" + word + "<br>" + "Index of 'n' " + firstword +"<br>" 
  console.log(midword);
  document.write("String :" +word + "<br>" +"Character at index 3 = "+ midword +"<br>")
 
-//Question no 7
-// var firstname =prompt("Enter your first name");
-// var lastname =prompt("Enter your last name");
-// var fullName = firstname.concat(" " , lastname)
-// document.write("<h2>Welcome "+ fullName +"<br>")
+//Question no 6
+var firstname =prompt("Enter your first name");
+var lastname =prompt("Enter your last name");
+var fullName = firstname.concat(" " , lastname)
+document.write("<h2>Welcome "+ fullName +"<br>")
 
-//Question no 8
+//Question no 7
 var sentence ="Hyderabad is the city of Pakistan";
 document.write("<h1>Hyderabad</h1> ")
 document.write("<h4>"+sentence+"</h4>"+"<br>")
 document.write("<h1>Replace sentence</h1>")
 var newsent =  sentence.replace("Hyder","Islam")
 document.write("<h4>"+newsent+"</h4>"+"<br>")
-// Question no 9
+// Question no 8
 var text ="Animals are kind and helpful living beings andt hey make our world beautiful. Some animals live in homes and are called pets, and others live in forests and oceans. We should love animals and care for them and protect them from harm"
 document.write("<h1>Animals</h1>")
 document.write("<p>"+text+"</p>"+"<br>")
 document.write("<h1>Replace sentence</h1>")
 var newtext =text.replaceAll("and" ,"&")
 document.write("<p>"+newtext+"</p>"+"<br>")
- // Question no 10
+ // Question no 9
 var str = "472";
 document.write("Value: " + str + "<br>");
 document.write("Type: " + typeof str + "<br>");
@@ -58,14 +58,26 @@ var num = Number(str);
 document.write("Value: " + num + "<br>");
 document.write("Type: " + typeof num +"<br>");
 
-//Question no 11
+//Question no 10
 var user =prompt("Enter a word which you like")
 var newuser = user.toUpperCase()
 document.write(newuser+"<br>")
-//Question no 12
+//Question no 11
 var fruitname =prompt("Enter a fruit")
 var newfruit =fruitname.slice(0,1)
 var remainingChar=fruitname.slice(0)
 var titleCase = newfruit.toUpperCase() + remainingChar.toLowerCase()
 document.write("My favourite fruit is " +titleCase +"<br>")
-// Question no 13
+// Question no 12
+var num = 35.36;
+
+// Convert number to string
+var numString = num.toString();
+
+// Remove the dot
+var result = numString.replace(".", "");
+
+// Display result
+document.write("Number: " + num + "<br>");
+document.write("Result: " + result);
+
