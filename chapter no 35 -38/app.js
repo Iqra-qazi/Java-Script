fb4a976fdbcffd28b2207a95d5a6778ff5846129
Iqra-qazi <iqraq577@gmail.com>
2026-02-05 22:45:03 +0500

    js

--- chapter no 35 -38/app.js
+++ chapter no 35 -38/app.js
@@ -5,15 +5,15 @@ function date(){
 }
 date()
 //Question no 2
-var userName =prompt("Enter your name here") || "user";
-var lName = prompt("Enter your last name")
-var fullName=userName+lName
-function greet(fullName){
+// var userName =prompt("Enter your name here") || "user";
+// var lName = prompt("Enter your last name")
+// var fullName=userName+lName
+// function greet(fullName){
     
-console.log("Welcome " +fullName) ;
+// console.log("Welcome " +fullName) ;
 
-}
-greet(userName,lName)
+// }
+// greet(userName,lName)
 
 //Question no 3
 function sum(a,b){
@@ -35,3 +35,38 @@ function squares(num){
     
 }
 squares(4);
+// Question no 6
+function factorial(n){
+    result = 1
+    for(var i =1;i<=n;i++){
+result=result*i
+    }
+    return result;
+}
+console.log(factorial(10));
+
+// Question no 7
+function counting(start,end){
+    var showcount=""
+    for(var i =start;i<=end;i++){
+showcount+=i+"";
+    }
+    return showcount;
+}
+document.write(counting(1,0))
+//Question no 8
+
+function calculateHyp(base, perp) {
+  function calculateSquare(x) {
+    return x ** 2;
+  }
+  var hypSquare = calculateSquare(base) + calculateSquare(perp)
+  return hypSquare
+}
+console.log(calculateHyp(2,3));
+//Question no 9
+function rectangleArea(height,weight){
+    return height*weight;
+}
+console.log(rectangleArea(2,2))
+//Question no 10
